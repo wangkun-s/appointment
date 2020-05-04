6e0a8ee647da4f99754a09fa57b197cd4ff93ddc
wangkun <wangkun>
2020-05-04 23:34:59 +0800

    appointment

--- web/src/components/usermessage/page7.vue
+++ web/src/components/usermessage/page7.vue
@@ -1,8 +1,8 @@
-<template>
-  <table style="border: 1px solid #99a9bf;width: 1000px;margin-left: 100px">
-    <div class="container-fluid">
-      <div class="row" style="height: 50px">
-        <div class="col-md-12" style="text-align: center">
+<template >
+  <div id="FullOrderDetailBox">
+    <div style="border: 1px solid #99a9bf;width: 1000px;margin-left: 100px">
+      <div class="row" style="height: 50px" >
+        <div class="col-md-12" style="text-align: center" >
           医好连锁口腔医院
         </div>
       </div>
@@ -11,7 +11,7 @@
           基本信息：
         </div>
       </div>
-      <div class="row" style="height: 50px">
+      <div class="row" style="height: 50px"  id="pdfCentent">
         <div class="col-md-3">
           姓名：
         </div>
@@ -68,13 +68,34 @@
           医嘱：
         </div>
       </div>
+      <el-button type="danger"
+                 @click="PrintClick">导出PDF</el-button>
     </div>
-  </table>
+  </div>
 </template>
+
 
 <script>
     export default {
-        name: ""
+        name: "page7",
+      data () {
+        return {
+          htmlTitle: "PDF名称",
+          nowTime: "",
+        }
+      },
+      methods:{
+        PrintClick() {
+          // window.print();//打印全部
+          var oldHtml = document.body.innerHTML;//将body内容先行存储
+          var printbox = document.getElementById("FullOrderDetailBox").innerHTML;//再将所要打印区域内容赋值给body
+          document.body.innerHTML = printbox;//再将所要打印区域内容赋值给body
+          window.print();//调用全部打印事件
+          document.body.innerHTML = oldHtml;//将body内容再返回原页面
+          window.location.reload();//打印取消后刷新页面防止按钮不能点击
+        }
+
+      }
     }
 </script>
 
